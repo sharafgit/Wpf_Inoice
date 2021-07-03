123a34014600dbd4a6fdb8a70a78acec5b6947cd
sharafgit <sharaf.for@gmail.com>
2021-07-03 02:31:37 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_Inoice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void print_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.IsEnabled = false;
+                PrintDialog printDialog = new PrintDialog();
+                if (printDialog.ShowDialog() == true)
+                {
+                    printDialog.PrintVisual(print, "Invoice");
+                }
+            }
+            finally
+            {
+
+                this.IsEnabled = true;
+            }
+        }
+
+        private void view_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Hello:-(Name:{CustomerName.Text})-(Date:{Date.Text} )-(Company: {company.Text})-(Count:{count.Text})-(Price:{price.Text})-(Item Name:{itemName.Text})");
+        }
+    }
+}
